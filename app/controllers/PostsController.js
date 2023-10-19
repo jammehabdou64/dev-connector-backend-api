7cdc4be8fe343899834df46865c63fac17534c18
Abdou jammeh <jammehabdou64@gmail.com>
2023-10-19 08:46:16 +0000

    added delete post functionality

--- app/controllers/PostsController.js
+++ app/controllers/PostsController.js
@@ -42,6 +42,30 @@ class PostsController {
     const save = await postRequest.like();
     return res.json({ message: save, success: true });
   }
+
+  async myPost(req, res, next) {
+    const posts = await Post.find()
+      .where("author", req.id)
+      .populate("author", ["name", "email", "avatar"]);
+    return res.json({ message: posts, success: true });
+  }
+
+  async destroy(req, res, next) {
+    const post = await Post.findById(req.params.post);
+    if (!post) {
+      return res.json({ message: null, success: false });
+    }
+
+    if (post.author.toString() === req.id) {
+      const deletePost = await Post.findByIdAndDelete(post._id.toString());
+      return deletePost
+        ? res.json({ message: "Post deleted successfully", success: true })
+        : res.json({ message: "Sorry, an error occur ", success: false });
+    }
+    if (!post) {
+      return res.json({ message: null, success: false });
+    }
+  }
 }
 
 module.exports = new PostsController();
--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,8 @@ const {
   show,
   like,
   comment,
+  myPost,
+  destroy,
 } = require("../app/controllers/PostsController");
 const { asyncHandler } = require("../utils");
 
@@ -12,5 +14,7 @@ router.route("/").get(asyncHandler(index)).post(asyncHandler(store));
 router.route("/:post").get(asyncHandler(show));
 router.route("/like/:post").put(asyncHandler(like));
 router.route("/comment/:post").put(asyncHandler(comment));
+router.route("/myposts/:user").get(asyncHandler(myPost));
+router.route("/:post").delete(asyncHandler(destroy));
 
 module.exports = router;
